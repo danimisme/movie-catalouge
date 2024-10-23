3755542dc93915e129c3b0b6eae7e11e451c71b6
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-10-23 23:04:17 +0700

    create drawer element

---
+++ src/scripts/views/app.js
@@ -0,0 +1,23 @@
+import DrawerInitiator from '../utils/drawer-initiator.js';
+
+class App {
+  constructor({ button, drawer, content }) {
+    this._button = button;
+    this._drawer = drawer;
+    this._content = content;
+
+    this._initialAppShell();
+  }
+
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content,
+    });
+  }
+
+}
+
+export default App;
