4ee22edfd8593f00b7d8fc1c8fc27e5423b4f908
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2024-11-03 14:28:04 +0700

    render movies data with template

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,16 +1,20 @@
 import TheMovieDbSource from '../../data/themoviedb-source';
+import { createMovieDetailTemplate } from '../templates/template-creator';
+import UrlParser from '../../routes/url-parser';
 
 const Detail = {
   async render() {
     return `
-        <h2>Detail Page</h2>
-      `;
+      <div id="movie" class="movie"></div>
+    `;
   },
 
   async afterRender() {
     // Fungsi ini akan dipanggil setelah render()
-    const movie = await TheMovieDbSource.detailMovie(id);
-    console.log(movie);
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const movie = await TheMovieDbSource.detailMovie(url.id);
+    const movieContainer = document.querySelector('#movie');
+    movieContainer.innerHTML = createMovieDetailTemplate(movie);
   },
 };
 
